ca8357366cef63cad904596fde7dcf88ce2c0763
grrtbrtr <gerrit@mrmoose.be>
2018-06-25 16:37:24 +0200

    Removed color field from persona model since it's no longer in use

--- src/js/app/models/Persona.js
+++ src/js/app/models/Persona.js
@@ -1,30 +1,3 @@
-const rgbComponentToHex = (component) => {
-  let hex = Math.
-    round(component).
-    toString(16); // eslint-disable-line no-magic-numbers
-
-  if (hex.length === 1) { // eslint-disable-line no-magic-numbers
-    hex = `0${hex}`;
-  }
-
-  return hex;
-}
-
-const generateColor = () => {
-  const MAX_CHANNEL_VALUE = 255;
-
-  let red = Math.floor(Math.random() * (MAX_CHANNEL_VALUE + 1));
-  let green = Math.floor(Math.random() * (MAX_CHANNEL_VALUE + 1));
-  let blue = Math.floor(Math.random() * (MAX_CHANNEL_VALUE + 1));
-
-  // Turn colors into pastel
-  red = (red + MAX_CHANNEL_VALUE) / 2;
-  green = (green + MAX_CHANNEL_VALUE) / 2;
-  blue = (blue + MAX_CHANNEL_VALUE) / 2;
-
-  return `#${rgbComponentToHex(red)}${rgbComponentToHex(green)}${rgbComponentToHex(blue)}`; // eslint-disable-line max-len
-}
-
 class Persona {
 
   /**
@@ -46,14 +19,6 @@ class Persona {
       company: null,
       title: null
     };
-
-    let color = generateColor();
-
-    while (Persona.usedColors.indexOf(color) > -1) {
-      color = generateColor();
-    }
-    this.color = color;
-    Persona.usedColors.push(color);
   }
 
   /**
@@ -68,7 +33,5 @@ class Persona {
   }
 
 }
-
-Persona.usedColors = [];
 
 export default Persona;
